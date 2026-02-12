0e7ec2848d8c6e0c683313420d582d85d0ff1b95
qinwenjing7-cyber <qinwenjing7@gmail.com>
2026-02-12 16:50:05 +0800

    Refactor streamlit app structure and comments

--- 项目实战之AI应用/01.sreamlit.py
+++ 项目实战之AI应用/01.sreamlit.py
@@ -20,7 +20,7 @@ st.set_page_config(
 st.write('你好，这是我们团队做的新型电力系统预警平台')
 
 #图片
-st.image('./resources/全年供需量图.jpg')
+st.image('resources/全年供需量图.jpg')
 
 #音频
 #st.audio('')
@@ -37,6 +37,7 @@ st.logo('resources/图片1.png')
 #st.table('')
 
 #输入框
+
 
 
 
